80bb1e69e02243439bac92848897f40da77216d0
RobertNateCrummett <100966812+RobertCrummett@users.noreply.github.com>
2022-06-09 19:59:10 -0700

    Update Bartlett.m

--- Bartlett.m
+++ Bartlett.m
@@ -15,8 +15,5 @@ function [M, sp2] = Bartlett(varargin)
 
     sp2 = (1/(N - k))*sum((n - 1).*(s.^2));
 
-    numerator = (N-k)*log(sp2) - sum((n -1).*log(s.^2));
-    denominator = 1 + (1/(3*(k - 1)))*(sum(1./(n - 1)) - 1/(N - k));
-
-    M = numerator/denominator;
+    M = ((N-k)*log(sp2) - sum((n -1).*log(s.^2)))./(1 + (1/(3*(k - 1)))*(sum(1./(n - 1)) - 1/(N - k)));
 end
