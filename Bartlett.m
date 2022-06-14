3bf8352adb79c516be1f6b14af27547a0548cce7
RobertNateCrummett <100966812+RobertCrummett@users.noreply.github.com>
2022-06-14 08:36:21 -0700

    Add files via upload

--- Bartlett.m
+++ Bartlett.m
@@ -2,6 +2,7 @@ function [M, sp2] = Bartlett(varargin)
     % credit to Wikipedia
     % this get the chisqstat exactly, unlike Dr. Chave's function
     % R N Crummett 6/6/2022
+
     k = nargin;
     n = zeros(1,k);
     s = zeros(1,k);
@@ -15,5 +16,5 @@ function [M, sp2] = Bartlett(varargin)
 
     sp2 = (1/(N - k))*sum((n - 1).*(s.^2));
 
-    M = ((N-k)*log(sp2) - sum((n -1).*log(s.^2)))./(1 + (1/(3*(k - 1)))*(sum(1./(n - 1)) - 1/(N - k)));
+    M = ((N-k)*log(sp2) - sum((n - 1).*log(s.^2)))./(1 + (1/(3*(k - 1)))*(sum(1./(n - 1)) - (1/(N - k))));
 end
